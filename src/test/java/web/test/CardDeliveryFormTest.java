62479b39f86db8379e19c2e69a16cc3443a738f6
Natalya Pletneva <nntalita@gmail.com>
2022-02-07 21:43:17 +0300

    initial commit

---
+++ src/test/java/web/test/CardDeliveryFormTest.java
@@ -0,0 +1,84 @@
+package web.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import web.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryFormTest {
+
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+    @Test
+    public void shouldFormTest() throws InterruptedException {
+
+
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String planningDate = DataGenerator.generateDate(4);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue(DataGenerator.generateLastName("ru") + " " + DataGenerator.generateFirstName("ru"));
+        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone("ru"));
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $("[data-test-id='success-notification'] [class='notification__title']").shouldHave(text("Успешно"),
+                Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно запланирована на " + planningDate));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        planningDate = DataGenerator.generateDate(7);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $(".button__text").click();
+        $(byText("Перепланировать")).shouldBe(visible, Duration.ofSeconds(15)).click();
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + planningDate));
+    }
+
+    @Test
+    public void shouldFormTestSameDate() throws InterruptedException {
+
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String planningDate = DataGenerator.generateDate(4);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue(DataGenerator.generateLastName("ru") + " " + DataGenerator.generateFirstName("ru"));
+        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone("ru"));
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $("[data-test-id='success-notification'] [class='notification__title']").shouldHave(text("Успешно"),
+                Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно запланирована на " + planningDate));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        planningDate = DataGenerator.generateDate(4);
+        $("[data-test-id=date] input").setValue(planningDate);
+        $(".button__text").click();
+        $("[data-test-id=replan-notification] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на " + planningDate));
+    }
+
+}
