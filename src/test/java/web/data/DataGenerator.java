f5ce2fd6a84a345e4762ce1768103076dc6b85d6
Natalya Pletneva <nntalita@gmail.com>
2022-02-08 11:13:25 +0300

    formatted

--- src/test/java/web/data/DataGenerator.java
+++ src/test/java/web/data/DataGenerator.java
@@ -31,27 +31,10 @@ public class DataGenerator {
     }
 
     public static String generatePhone(String locale) {
-       final String phone = faker.phoneNumber().cellPhone();
-        return phone;
+        return faker.phoneNumber().cellPhone();
     }
     public static String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-//    public static class Registration {
-//        private Registration() {
-//        }
-//
-//        public static UserInfo generateUser(String locale) {
-//            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-//            // generateName(locale), generatePhone(locale)
-//            return user;
-//        }
-//    }
-//
-//    @Value
-//    public static class UserInfo {
-//        String city;
-//        String name;
-//        String phone;
-//    }
+
 }
